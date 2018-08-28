9b1069e1c4ee26f204e2411de76b927a966103c4
Imre Otto <0010bit@gmail.com>
2018-08-28 15:16:58 +0300

    Tenta log flag for GUI logs; TENTA_LOG_GUI

--- runtime/android/empty_tenta/meta_logging.h
+++ runtime/android/empty_tenta/meta_logging.h
@@ -43,12 +43,19 @@ namespace tenta {
 #define TENTA_HISTORY_OK_TO_LOG() false
 #endif
 
+#if TENTA_LOG_GUI_ENABLE == 1
+#define TENTA_GUI_OK_TO_LOG() true
+#else
+#define TENTA_GUI_OK_TO_LOG() false
+#endif
+
 #else // TENTA_LOG_ENABLE
 #define TENTA_DB_OK_TO_LOG() false
 #define TENTA_NET_OK_TO_LOG() false
 #define TENTA_CACHE_OK_TO_LOG() false
 #define TENTA_COOKIE_OK_TO_LOG() false
 #define TENTA_HISTORY_OK_TO_LOG() false
+#define TENTA_GUI_OK_TO_LOG() false
 #endif // TENTA_LOG_ENABLE
 
 #define TENTA_LOG_DB(severity) \
@@ -65,6 +72,9 @@ namespace tenta {
 
 #define TENTA_LOG_HISTORY(severity) \
     LAZY_STREAM(LOG_STREAM(severity), TENTA_HISTORY_OK_TO_LOG())
+
+#define TENTA_LOG_GUI(severity) \
+    LAZY_STREAM(LOG_STREAM(severity), TENTA_GUI_OK_TO_LOG())
 
 } // namespace tenta
 #endif /* TENTA_META_FS_META_LOGGING_H_ */
