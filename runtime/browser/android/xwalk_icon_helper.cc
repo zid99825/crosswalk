72a3c7a715c31e39a966b66398452be687b1540f
Imre Otto <otto.imre@gmail.com>
2020-02-24 10:22:52 +0200

    Choose/download largest favicon

--- runtime/browser/android/xwalk_icon_helper.cc
+++ runtime/browser/android/xwalk_icon_helper.cc
@@ -43,6 +43,9 @@ void XWalkIconHelper::DownloadIcon(const GURL& icon_url) {
 void XWalkIconHelper::DidUpdateFaviconURL(
     const std::vector<content::FaviconURL>& candidates) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+  content::FaviconURL largest_icon;
+  gfx::Size max_size;
+
   for (std::vector<content::FaviconURL>::const_iterator i = candidates.begin();
        i != candidates.end(); ++i) {
     if (!i->icon_url.is_valid())
@@ -50,15 +53,11 @@ void XWalkIconHelper::DidUpdateFaviconURL(
 
     switch (i->icon_type) {
       case content::FaviconURL::IconType::kFavicon:
-        if ( !_got_first_icon) {
-          // download only the first favicon
-          _got_first_icon = true;
-//        if (listener_) listener_->OnIconAvailable(i->icon_url);
-        // TODO(iotto) : Ask Clinet if favicons should be downloaded
-        web_contents()->DownloadImage(i->icon_url, true,  // Is a favicon
-                                      0,  // No maximum size
-                                      false,  // Normal cache policy
-                                      base::Bind(&XWalkIconHelper::DownloadFaviconCallback, _this_weak.GetWeakPtr()));
+        for ( auto& ico_size: i->icon_sizes) {
+          if ( (max_size.width() < ico_size.width()) || (max_size.height() < ico_size.height())) {
+            max_size = ico_size;
+            largest_icon = *i;
+          }
         }
         break;
       case content::FaviconURL::IconType::kTouchIcon:
@@ -72,6 +71,18 @@ void XWalkIconHelper::DidUpdateFaviconURL(
         break;
     }
   }
+
+  if (!_got_first_icon && largest_icon.icon_url.is_valid()) {
+    // download only the first favicon
+    _got_first_icon = true;
+//        if (listener_) listener_->OnIconAvailable(i->icon_url);
+    // TODO(iotto) : Ask Clinet if favicons should be downloaded
+    web_contents()->DownloadImage(largest_icon.icon_url, true,  // Is a favicon
+                                  0,  // No maximum size
+                                  false,  // Normal cache policy
+                                  base::Bind(&XWalkIconHelper::DownloadFaviconCallback, _this_weak.GetWeakPtr()));
+  }
+
 }
 
 void XWalkIconHelper::DidFinishNavigation(content::NavigationHandle* navigation_handle) {
@@ -89,7 +100,19 @@ void XWalkIconHelper::DownloadFaviconCallback(
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   if (http_status_code == 404 || bitmaps.size() == 0) return;
 
-  if (listener_) listener_->OnReceivedIcon(image_url, bitmaps[0]);
+  // find largest bitmap
+  gfx::Size max_size;
+  int bitmap_pos = 0;
+
+  for ( int i = 0; i < (int)original_bitmap_sizes.size(); ++i) {
+    const gfx::Size& bitmap_size = original_bitmap_sizes[i];
+
+    if ( (max_size.width() < bitmap_size.width()) || (max_size.height() < bitmap_size.height())) {
+      max_size = bitmap_size;
+      bitmap_pos = i;
+    }
+  }
+  if (listener_) listener_->OnReceivedIcon(image_url, bitmaps[bitmap_pos]);
 }
 
 }  // namespace xwalk
